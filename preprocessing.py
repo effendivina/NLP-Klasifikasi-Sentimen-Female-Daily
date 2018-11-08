3e41ee69ba6592ee4e3e6d7fa325fa03aa8869a8
Vina <effendivina@gmail.com>
2018-11-08 10:46:24 +0700

    punctuation dan stemming sastrawi

--- preprocessing.py
+++ preprocessing.py
@@ -1,20 +1,44 @@
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import readfile
 import re
 import sys
-import readfile
 import nltk.tokenize
 
-def preprocessing_word(word):
+
+def stemming(kalimat):
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    output   = stemmer.stem(kalimat)
+    return output
+
+def preprocessing_word(kalimat):
     #menghilangkan tanda baca
-    word = word.strip('\'"?!,.():;*')
+    kalimat_preprocess = []
+    for word in kalimat:
+        word = word.strip('\'"?!,.():;*')
 
-    # #mengkonversi huruf vocal lebih dari satu dan berurutan
-    word = re.sub(r'(.)\1+', r'\1\1', word)
+        # #mengkonversi huruf vocal lebih dari satu dan berurutan
+        word = re.sub(r'(.)\1+', r'\1', word)
 
-    # #menghilangkan tanda - & '
-    word = re.sub(r'(-|\')','',word)
+        # #menghilangkan tanda - & '
+        word = re.sub(r'(-|\')','',word)
 
-    return word
+        kalimat_preprocess.append(word)
+    return ' '.join(kalimat_preprocess)
 
-sentence = readfile.openarticle('Data-Train.csv')
-print((sentence[0]))                                  
+corpus,kelas  = readfile.openarticle('Data-Train.csv')
+
+kalimat_punct = []
+for kalimat in corpus:
+    x = preprocessing_word(kalimat)
+    kalimat_punct.append(x)
+
+kalimat_stem=[]
+for kalimat in kalimat_punct:
+    x = stemming(kalimat)
+    kalimat_stem.append(x)
+
+for kalimat in kalimat_stem:
+    print(kalimat)
+    print()
 
--- readfile.py
+++ readfile.py
@@ -4,11 +4,11 @@ def openarticle(doc_name):
     with open(doc_name, encoding="utf-8") as csvfile: 
         next(csvfile)
         rawArticles = csv.reader(csvfile, delimiter=',') 
-        articles = [] 
-        labels = []
+        perkata = [] #array perkata akan menyimpan semua kalimat menjadi perkata
+        perkalimat = [] #array perkalimat menyimpan semua kalimat menjadi perkalimat
+        kelas = []
         for row in rawArticles:
-            articles.append(row[3].lower()) 
-            labels.append(row[5].lower())
-    return articles, labels 
-
-
+            perkalimat=row[3].lower()
+            perkata.append(perkalimat.split())
+            kelas.append(row[5].lower())
+    return perkata, kelas 
